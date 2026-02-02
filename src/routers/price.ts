2f90dcaefdb503eff6cdb6380f383a286bfa619f
max180643 <max180643@gmail.com>
2026-02-02 21:04:53 +0700

    fix: :bug: implement caching for latest price data

--- src/routers/price.ts
+++ src/routers/price.ts
@@ -1,14 +1,24 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 import priceHandler from "../controllers/price";
 
+const cache: { data?: any; expiry?: number } = {};
+
 const priceRouters = async (app: FastifyInstance) => {
   app.get("/latest", async (request: FastifyRequest, reply: FastifyReply) => {
-    const data = await priceHandler();
+    const now = Date.now();
+    if (cache.data && cache.expiry && cache.expiry > now) {
+      reply.code(200).send(cache.data);
+      return;
+    }
 
-    reply.code(200).send({
+    const data = await priceHandler();
+    const response = {
       status: data.status,
       response: data.response,
-    });
+    };
+    cache.data = response;
+    cache.expiry = now + 60 * 1000; // 1 minute
+    reply.code(200).send(response);
   });
 };
 
